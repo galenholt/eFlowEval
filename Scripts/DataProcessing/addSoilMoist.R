2f72f92d67642185bff531ca274314dab16758ef
Galen Holt(Mac) <g.holt@deakin.edu.au>
2020-09-02 11:50:09 +1000

    Make a few different example versions of including time-series data, including different ways to process it and different ways to include it in the polygons

--- Scripts/DataProcessing/addSoilMoist.R
+++ Scripts/DataProcessing/addSoilMoist.R
@@ -1,10 +1,13 @@
 # Script to build on the ANAE without having to reprocess it
-
+library(here)
 library(tidyverse)
 library(sf)
 library(stars)
 
 # Argh. sort all this directory crap out later
+source(here('Scripts', 'DataProcessing', 'rastPolyJoin.R'))
+source(here('Scripts', 'DataProcessing', 'timeRoll.R'))
+
 
 myhome <- str_remove(path.expand("~"), "/Documents")
 datDir <- file.path(myhome, "Deakin University/QAEL - MER/Model/dataBase") # "C:/Users/Galen/Deakin University/QAEL - MER/Model/dataBase"
@@ -49,89 +52,166 @@ basin <- st_transform(basin, whichcrs)
 # Crop. Have to use as_points = FALSE or it crops to the raster centers, and misses stuff around the edges.
 lachSoil <- st_crop(soilMstars, filter(ltimCut, ValleyName == 'Lachlan'), as_points = FALSE)
 # check it worked
-plot(lachSoil[,,,1:4]) 
+plot(lachSoil[,,,1:4])
 
+
+# Let's set up a bbox for subsampled plotting without taking 8 million years
+bb = st_bbox(c(xmin = 144.4, ymin = -33.8, xmax = 144.6, ymax = -33.6), crs = whichcrs)
+
+# And quick plots of the ANAE and rasters
+anaeSub <- st_crop(lachAll, st_as_sfc(bb))
+plot(anaeSub[,'ANAE_DESC'])
+
+soilSub <- lachSoil[st_as_sfc(bb)]
+plot(soilSub[,,,10:13])
 
 # 1. Spatial average of raster into the ANAE polys at each timestep ---------------------------
 
-# First, make the stars into sf polygons
-# TODO: likely going to need to start overwriting objects to keep size down at some point. 
-# Though, this sf is going to be used for a few things, as is the raster
-soilSF <- st_as_sf(lachSoil, as_points = FALSE, merge = FALSE)
-str(soilSF)
+# Test the new function
+system.time(dailySMpolyavg <- rastPolyJoin(polysf = lachAll, rastst = lachSoil, grouper = 'SYS2', maintainPolys = TRUE))
+# 290 seconds with 280 timesteps in Lachlan
+  # There has GOT to be a way to speed this up...
+  # It's the grouped summarize that kills it, NOT the intersection. i think it
+  # might be the work sf is doing to put the polygons back together?
+    # TODO: see if it's faster to st_drop_geometry, do the summarize, and then join back to lachAll with SYS2?
 
-# Have to intersect with the ANAE polygons to get average.
-# Less fiddly (because it's one-to-one), and it ensures the averages area area
-# weighted (they're not with aggregate; see timePolyRastScratch for testing)
-intA_S <- st_intersection(lachAll, soilSF)
-# intF leads with the ANAE cols, and then has date cols all preceded by 'X'
+# unpack the list
+dailyPolyindex <- dailySMpolyavg[[2]]
+# at least for testing, don't overwrite, or have to run the whole thing again
+dailyPolySMavg <- dailySMpolyavg[[1]]
 
-# Get the averages into each ANAE ID at each time
-# This strips off the other ANAE cols, which is fine. Could do it earlier, but don't see a need?
-# TODO:: Again, rename to reduce size later on
-# 255 seconds with 221 cols
-system.time(avgA_S <- intA_S %>%
-              mutate(area = st_area(.)) %>%  
-              group_by(SYS2) %>%
-              summarize(across(starts_with("X"), ~weighted.mean(.x, as.numeric(area)))) %>% # averages across the cols with x in their name and gets weighted mean
-              # TODO:: is it faster to pre-allocate these cols? IE just cut to
-              # only those cols and then summarize everything?
-              # Tried it and it crashed, but maybe moving out of dplyr syntax
-              # entirely would speed it up, but be a pain. across() finding
-              # the indices shouldn't be that slow, really, though. so the
-              # only issue is if it can't parallelize or something
-              # Same as above, but keeps all the other columns. Way slower
-              # summarize(across(starts_with("X"), ~weighted.mean(.x, as.numeric(area))), 
-              #           across(-c(starts_with("X"), Shape), first)) %>% # st_area returns a units object, which is good, but breaks weighted.mean
-              ungroup())
+# Check ordering
+all(dailyPolyindex$SYS2 == lachAll$SYS2)
 
-# So, that's basically all the data processing for this one, but for format
-# consistency and letting time be time, let's put it back into a `stars` array
+# Test plot
+dailyPolySub <- dailyPolySMavg[st_as_sfc(bb)]
+plot(dailyPolySub[,,10:13])
 
-# TODO:: do the checks here that the new SYS2 variable fixes the duplicated SYSIDs. 
-# then, make the stars with linking vector for confirming indexing
-# Then, on to the different rollers
-
-# It has been re-sorted for some unknown stupid dplyr reason. Make sure it is sorted to match lachAll
-avgA_S <- avgA_S %>% arrange(SYS2)
-lachAll <- lachAll %>% arrange(SYS2)
-# Check
-all(avgA_S$SYS2 == lachAll$SYS2)
-
-# And, still, save the indexer column (is it possible to glue this back on as a dimension? Probably not, because it's parallel to shape)
-avgA_Sindex <- avgA_S$SYS2
-# at the least, it'll allow me to see if things have gotten jumbled between shape and sys2
-
-# Turn back into a stars
-avgA_Stars <- avgA_S %>% 
-  select(-SYS2) %>%
-  st_as_stars() %>%
-  merge()
-# avgA_Stars
-
-# change the time dimension
-st_dimensions(avgA_Stars)[2] <- st_dimensions(lachSoil)[3]
-st_dimensions(avgA_Stars)[2] # yup, though it's still called X1?
-names(st_dimensions(avgA_Stars))[2] <- names(st_dimensions(lachSoil))[3]
-# and change the name of the attribute
-names(avgA_Stars) <- names(lachSoil)
-
-# avgA_Stars
-# plot(avgA_Stars[,,50:51])
-
+# ggplot is a bigger pain, skip for now, we just want to look at things
+# testDaily <- ggplot() + 
+#   geom_stars(data = dailyPolySub[,,10:13], aes(fill = sm_pct)) +
+#   facet_grid(cols = as.character)
+# testDaily
 
 # Rolling through time over the spatial polygons ---------------------
 
-# Set up a stars object from the daily soil moistures in the polygons
-polyTimeSmax_10 <- avgA_Stars 
+# Set up a new stars object from the daily soil moistures in the polygons
+polyTimeSmax_10 <- dailyPolySMavg 
 
 # Get a 10-day rolling max for each polygon
-polyTimeSmax_10[[1]] <- timeRoll(polyTimeSmax_10[[1]], FUN = RcppRoll::roll_max, rolln = 10, align = 'right')
+system.time(polyTimeSmax_10[[1]] <- timeRoll(polyTimeSmax_10[[1]], 
+                                             FUN = RcppRoll::roll_max, 
+                                             rolln = 10, 
+                                             align = 'right',
+                                             na.rm = TRUE))
 
+# Half a second?? That's great!
 
+# Test plot
+max10PolySub <- polyTimeSmax_10[st_as_sfc(bb)]
+plot(max10PolySub[,,10:13])
 
 # Rolling through time on the raster, then putting into polygons --------
 
 # Could probably streamline with the dailies in the first section, but for now
 rastTimeSmean_7 <- lachSoil
-rastTimeSmean_7[[1]] <- timeRoll(rastTimeSmean_7[[1]], FUN = RcppRoll::roll_mean, rolln = 7, align = 'right')
+system.time(rastTimeSmean_7[[1]] <- timeRoll(rastTimeSmean_7[[1]],
+                                             FUN = RcppRoll::roll_mean, 
+                                             rolln = 7, 
+                                             align = 'right',
+                                             na.rm = TRUE))
+# 0.7 seconds
+# Test plot the raster itself
+rastweekSub <- rastTimeSmean_7[st_as_sfc(bb)]
+plot(rastweekSub[,,,10:13])
+
+
+# Now, put those time-means into polygons
+system.time(weeklySMpoly <- rastPolyJoin(polysf = lachAll, rastst = rastTimeSmean_7, grouper = 'SYS2', maintainPolys = TRUE))
+
+
+# unpack the list
+weeklyPolyindex <- weeklySMpoly[[2]]
+# at least for testing, don't overwrite, or have to run the whole thing again
+weeklyRastavgSMpoly <- weeklySMpoly[[1]]
+
+# Check ordering
+all(weeklyPolyindex$SYS2 == lachAll$SYS2)
+
+# Test plot
+weekmeanSub <- weeklyRastavgSMpoly[st_as_sfc(bb)]
+plot(weekmeanSub[,,10:13])
+
+
+
+# Split the polygons by the rasters to get smaller scale ------------------
+
+# Let's do this with the dailies, because then it would be easy to, for example, get a rolling min or something.
+
+# Test the new function
+system.time(dailySMpolysplit <- rastPolyJoin(polysf = lachAll, 
+                                             rastst = lachSoil, 
+                                             grouper = 'SYS2', 
+                                             maintainPolys = FALSE))
+# 290 seconds with 280 timesteps in Lachlan
+# There has GOT to be a way to speed this up...
+# It's the grouped summarize that kills it, NOT the intersection. i think it
+# might be the work sf is doing to put the polygons back together?
+# TODO: see if it's faster to st_drop_geometry, do the summarize, and then join back to lachAll with SYS2?
+
+# unpack the list
+dailyPolySplitindex <- dailySMpolysplit[[2]]
+# at least for testing, don't overwrite, or have to run the whole thing again
+dailyPolySMsplit <- dailySMpolysplit[[1]]
+
+# Check ordering
+  # This SHOULD be false
+all(dailyPolySplitindex$SYS2 == lachAll$SYS2)
+
+# Test plot
+dailyPolySplitSub <- dailyPolySMsplit[st_as_sfc(bb)]
+plot(dailyPolySplitSub[,,10:13])
+
+
+# all the test plots in one place -----------------------------------------
+  #including making the subsets and bbox, so we can use something different here, or comment out above
+
+# it's a little annoying that st_crop is a hard crop, but the [] cropping lets
+# the polys hang over, and so we get different extents. So doing the polygon
+# crops a bit different than above, so we DO get the same extents
+
+# Let's set up a bbox for subsampled plotting without taking 8 million years
+bb = st_bbox(c(xmin = 144.4, ymin = -33.8, xmax = 144.6, ymax = -33.6), crs = whichcrs)
+
+# ANAE
+anaeSub <- st_crop(lachAll, st_as_sfc(bb))
+plot(anaeSub[,'ANAE_DESC'])
+
+# Soil moisture raster
+soilSub <- lachSoil[st_as_sfc(bb)]
+plot(soilSub[,,,10:13])
+
+# Daily sm in the polygons
+dailyPolySub <- st_crop(st_as_sf(dailyPolySMavg[,,10:13]), st_as_sfc(bb))
+plot(dailyPolySub)
+
+# 10-day max in the polygons directly
+max10PolySub <- st_crop(st_as_sf(polyTimeSmax_10[,,10:13]), st_as_sfc(bb))
+plot(max10PolySub)
+
+# Weekly mean in the raster (as a raster still)
+rastweekSub <- rastTimeSmean_7[st_as_sfc(bb)]
+plot(rastweekSub[,,,10:13])
+
+# Weekly mean put into polygons
+weekmeanSub <- st_crop(st_as_sf(weeklyRastavgSMpoly[,,10:13]), st_as_sfc(bb))
+plot(weekmeanSub)
+
+# and the split by raster units
+dailyPolySplitSub <- st_crop(st_as_sf(dailyPolySMsplit[,,10:13]), st_as_sfc(bb))
+plot(dailyPolySplitSub)
+
+
+
+
+
