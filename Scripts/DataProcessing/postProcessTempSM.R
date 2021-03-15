13bf4287fa104224906eec11e9771a529bf0102d
Galen Holt <galen.holt@gmail.com>
2021-03-15 13:28:08 +1100

    pulled the matching of the SM and temp datasets out of the stricture headers and into its own script

---
+++ Scripts/DataProcessing/postProcessTempSM.R
@@ -0,0 +1,72 @@
+# script to clean up the demo sm and temp data. Much of this is to make the two datasets match up
+
+# Quick strict development
+
+library(here)
+library(tidyverse)
+library(sf)
+library(stars)
+
+# Argh. sort all this directory crap out later
+# Trying to at least separate scripts and functions, looking towards library
+# source(here('Functions', 'rastPolyJoin.R'))
+# source(here('Functions', 'timeRoll.R'))
+# source(here('Functions', 'helpers.R'))
+# source(here('Functions', 'unevenTimeMult.R'))
+
+
+myhome <- str_remove(path.expand("~"), "/Documents")
+datDir <- file.path(myhome, "Deakin University/QAEL - MER/Model/dataBase") # "C:/Users/Galen/Deakin University/QAEL - MER/Model/dataBase"
+
+datOut <- "datOut"
+
+
+# Read in soil moisture in ANAEs--------------------------------------------------
+
+load(file.path(datOut, 'lachSoilprocessedAllOut.rdata'))
+
+# Read in soil temp in ANAEs --------------------------------------------
+
+load(file.path(datOut, 'lachSoilTempprocessedAllOut.rdata'))
+# 
+
+# Temp; cut after pull out of the data processing script 
+rm(soilMoistMin5, soilMoistMin42, soilTempMax28)
+
+
+# # So, soil moist goes to Oct 2020 (currently)
+# max(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# # While temp goes to Dec 28 2019 (not sure why I didn't get the last few days of 2019)
+# max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+
+# So, need to cut moist back to match temp or the matched logicals barfs
+# Re-write into self, or end up using a ton of memory
+tempmax <- max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+dailyPolySMavg <- filter(dailyPolySMavg, time <= tempmax) # Check safety, had trouble with filter previously, but this SHOULD be ok since contiguous
+# 
+# dailyPolySMavg
+# max(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+
+moistmin <- min(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# And, they're off by 12 hours. argh. for now, just toss noon on dec 31 2013 in the temp too
+dailyPolyTempavg <- filter(dailyPolyTempavg, time >= moistmin) # Check safety, had trouble with filter previously, but this SHOULD be ok since contiguous
+# dailyPolyTempavg
+# testlogic <- dailyPolySMavg < dailyPolyTempavg
+# testlogic
+# 
+# max(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+# 
+# min(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# min(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+
+save(dailyPolyTempavg,
+     file = file.path(datOut, 'lachTempMatched.rdata'))
+
+save(dailyPolySMavg,
+     file = file.path(datOut, 'lachSMMatched.rdata'))
+
+
+
+
--- Scripts/Strictures/lippia.R
+++ Scripts/Strictures/lippia.R
@@ -47,47 +47,62 @@ datDir <- file.path(myhome, "Deakin University/QAEL - MER/Model/dataBase") # "C:
 datOut <- "datOut"
 
 
+# Read in just the ANAEs
+load(file.path(datOut, 'lachAll.rdata'))
+lachAll <- st_transform(lachAll, 4326) # WHY ISN"T THIS LIKE THIS ALREADY?
+
 # Read in soil moisture in ANAEs--------------------------------------------------
 
-load(file.path(datOut, 'lachSoilprocessedAllOut.rdata'))
+load(file.path(datOut, 'lachSMMatched.rdata'))
 
 # Read in soil temp in ANAEs --------------------------------------------
 
-load(file.path(datOut, 'lachSoilTempprocessedAllOut.rdata'))
+load(file.path(datOut, 'lachTempMatched.rdata'))
 # 
 
-# Temp; cut after pull out of the data processing script 
-rm(soilMoistMin5, soilMoistMin42, soilTempMax28)
 
 
-# # So, soil moist goes to Oct 2020 (currently)
-# max(st_get_dimension_values(dailyPolySMavg, which = 'time'))
-# # While temp goes to Dec 28 2019 (not sure why I didn't get the last few days of 2019)
-# max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
-
-# So, need to cut moist back to match temp or the matched logicals barfs
-# Re-write into self, or end up using a ton of memory
-tempmax <- max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
-dailyPolySMavg <- filter(dailyPolySMavg, time <= tempmax) # Check safety, had trouble with filter previously, but this SHOULD be ok since contiguous
+# # Read in soil moisture in ANAEs--------------------------------------------------
 # 
-# dailyPolySMavg
-# max(st_get_dimension_values(dailyPolySMavg, which = 'time'))
-# max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
-
-moistmin <- min(st_get_dimension_values(dailyPolySMavg, which = 'time'))
-# And, they're off by 12 hours. argh. for now, just toss noon on dec 31 2013 in the temp too
-dailyPolyTempavg <- filter(dailyPolyTempavg, time >= moistmin) # Check safety, had trouble with filter previously, but this SHOULD be ok since contiguous
-# dailyPolyTempavg
-# testlogic <- dailyPolySMavg < dailyPolyTempavg
-# testlogic
-
-
-
-# max(st_get_dimension_values(dailyPolySMavg, which = 'time'))
-# max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+# load(file.path(datOut, 'lachSoilprocessedAllOut.rdata'))
 # 
-# min(st_get_dimension_values(dailyPolySMavg, which = 'time'))
-# min(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+# # Read in soil temp in ANAEs --------------------------------------------
+# 
+# load(file.path(datOut, 'lachSoilTempprocessedAllOut.rdata'))
+# # 
+# 
+# # Temp; cut after pull out of the data processing script 
+# rm(soilMoistMin5, soilMoistMin42, soilTempMax28)
+# 
+# 
+# # # So, soil moist goes to Oct 2020 (currently)
+# # max(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# # # While temp goes to Dec 28 2019 (not sure why I didn't get the last few days of 2019)
+# # max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+# 
+# # So, need to cut moist back to match temp or the matched logicals barfs
+# # Re-write into self, or end up using a ton of memory
+# tempmax <- max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+# dailyPolySMavg <- filter(dailyPolySMavg, time <= tempmax) # Check safety, had trouble with filter previously, but this SHOULD be ok since contiguous
+# # 
+# # dailyPolySMavg
+# # max(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# # max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+# 
+# moistmin <- min(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# # And, they're off by 12 hours. argh. for now, just toss noon on dec 31 2013 in the temp too
+# dailyPolyTempavg <- filter(dailyPolyTempavg, time >= moistmin) # Check safety, had trouble with filter previously, but this SHOULD be ok since contiguous
+# # dailyPolyTempavg
+# # testlogic <- dailyPolySMavg < dailyPolyTempavg
+# # testlogic
+# 
+# 
+# 
+# # max(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# # max(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
+# # 
+# # min(st_get_dimension_values(dailyPolySMavg, which = 'time'))
+# # min(st_get_dimension_values(dailyPolyTempavg, which = 'time'))
 # -------------------------------------------------------------------------
 
 # Make stars objects to test against relevant to the strictures -----------
