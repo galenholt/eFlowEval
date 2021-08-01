59373b79ec60fe96e0d4c888268831d30eb79907
Galen Holt <galen.holt@gmail.com>
2021-08-01 21:36:14 +1000

    new rastPolyJoin that can chunk up single ANAEs if they're too large.

--- Functions/rastPolyJoin.R
+++ Functions/rastPolyJoin.R
@@ -1,7 +1,9 @@
 # Function to intersect raster and polygons and get spatial average and return stars
 
 rastPolyJoin <- function(polysf, rastst, grouper = 'UID', FUN = weighted.mean,  
-                         maintainPolys = TRUE, na.replace = NA, whichcrs = 3577) {
+                         maintainPolys = TRUE, na.replace = NA, whichcrs = 3577, 
+                         maxPixels = 100000,
+                         pixelsize = NA) {
   # polysf is a spatial polygon simple feactures object
   # rastst is a raster brick in stars format
   # grouper is a single grouping variable to identify single polygons in polysf
@@ -18,35 +20,56 @@ rastPolyJoin <- function(polysf, rastst, grouper = 'UID', FUN = weighted.mean,
   # but the ... syntax isn't playing nice for column names in the data frame
   # that are calculated internal to this function, so this assumption is a
   # workaround
+  # maxPixels and pixelsize are used to bring the raster in in chunks if it's too big
   
   # turn raster into polygon sf object, time as columns
     # This can take a really long time
   
-  # rastSF <- tryCatch()
+  # The conditional here avoids using parallel overhead and the grid
+  # intersection if smaller than maxPix. It would work with just the second
+  # part, but it's about 0.15 seconds slower per polygon
+  # Figure out how many raster pixels we're about to read in
+    # stars reads in a raster matching the bbox of the whole polygon dataframe (whether that's one or many polygons)
   
-  rastSF <- st_as_sf(rastst, as_points = FALSE, merge = FALSE, na.rm = FALSE)
+    bbarea <- as.numeric(st_area(st_as_sfc(st_bbox(polysf))))
+    pixneeded <- bbarea/pixelsize
+
   
-  # replace NA?
-  if (!is.na(na.replace)) {
-    rastSF[is.na(rastSF)] <- na.replace
+  # If less than maxPixels are needed, just get the intersection directly
+      # also if I didn't pass in a pixelsize or maxpixels
+    # rpintersect is just a wrapper of st_intersection with a bunch of read-in and transform boilerplate so it works consistently
+  if (is.na(pixneeded) | is.na(maxPixels) | (pixneeded <= maxPixels)) {
+    intPR <- rpintersect(singlesf = polysf, singleraster = rastst, 
+                         na.replace = na.replace, whichcrs = 3577)
+  } else if (pixneeded > maxPixels) {
+    # make a grid that breaks up the polygon (or is just the bb if the number of pixels is small enough)
+    grid <- st_make_grid(polysf, n = ceiling(sqrt(pixneeded/maxPixels)))
+    # Now, the intersection gets the polysf split by grids (and drops grids with no polysf in them)
+    gridsf <- st_intersection(polysf, grid) 
+    
+    # Now, foreach over each of the polygon chunks and bind_rows back together
+    # The group_by below for the stats means it doesn't matter that the gridding puts in extra 'cuts'
+    # and we put it back together here (rather than keep going all the way
+    # through the stats) because averages would require careful weighting if we
+    # aren't averaging over the whole anae polygon
+    
+    intPR <- foreach(r = 1:nrow(gridsf),
+                     .combine = bind_rows,
+                     .multicombine = TRUE) %dopar% {
+                       # Get the grid-cut polygon r
+                       thissmall <- gridsf[r,]
+                       # crop the raster to JUST this grid-cut polygon
+                       smallcrop <- st_crop(rastst, thissmall, as_points = FALSE)
+                       
+                       # Use the core rpintersect function to do the intersection
+                       intPRsmall <- rpintersect(singlesf = thissmall, singleraster = smallcrop, 
+                                                 na.replace = na.replace, whichcrs = 3577)
+                       # Return to be row-bound
+                       intPRsmall
+                     } # end foreach
   }
   
-  # Transform to correct crs.
-  # Doing this here, because if rastst is a proxy, we won't be able to transform until it's read in as st_as_sf
-  if (st_crs(rastSF)$epsg != whichcrs) {
-    rastSF <- st_transform(rastSF, whichcrs) %>%
-      st_make_valid()
-  }
-  # ensure the polygons match
-  if (st_crs(polysf)$epsg != whichcrs) {
-    polysf <- st_transform(polysf, whichcrs) %>%
-      st_make_valid()
-  }
-  
-  # Have to intersect with the polygons to get average.
-  # Less fiddly (because it's one-to-one), and it ensures the averages are area
-  # weighted (they're not with aggregate; see timePolyRastScratch for testing)
-  intPR <- st_intersection(polysf, rastSF)
+
   # test <- 1
   if (maintainPolys) {
     # Get the averages into each ANAE ID at each time
@@ -128,3 +151,43 @@ rastPolyJoin <- function(polysf, rastst, grouper = 'UID', FUN = weighted.mean,
   return(list(avgPRStars, avgPRindex))
   
 }
+
+
+
+
+# The core intersection function used by rastPolyJoin ------------------------------------------
+
+# THIS IS THE CORE FUNCTION THAT does a single
+# polygon x raster intersect (or a whole sf dataframe) and ensures transforms,
+# etc
+rpintersect <- function(singlesf, singleraster, 
+                        na.replace = NA, whichcrs = 3577) {
+  
+  # read in the stars as sf polys
+    # I want to use merge = TRUE to reduce the number of polygons, but it ALSO merges the time dimension, which is bad.
+  rastSF <- st_as_sf(singleraster, as_points = FALSE, merge = FALSE, na.rm = FALSE)
+  
+  # replace NA?
+  if (!is.na(na.replace)) {
+    rastSF[is.na(rastSF)] <- na.replace
+  }
+  
+  # Transform to correct crs.
+  # Doing this here, because if rastst is a proxy, we won't be able to transform until it's read in as st_as_sf
+  if (st_crs(rastSF)$epsg != whichcrs) {
+    rastSF <- st_transform(rastSF, whichcrs) %>%
+      st_make_valid()
+  }
+  # ensure the polygons match
+  if (st_crs(singlesf)$epsg != whichcrs) {
+    singlesf <- st_transform(singlesf, whichcrs) %>%
+      st_make_valid()
+  }
+  
+  # Have to intersect with the polygons to get average.
+  # Less fiddly (because it's one-to-one), and it ensures the averages are area
+  # weighted (they're not with aggregate; see timePolyRastScratch for testing)
+  intersectedPR <- st_intersection(singlesf, rastSF)
+} 
+
+
