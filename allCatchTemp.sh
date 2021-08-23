fe2af16388d206299717fb72755116b63fc876f9
Galen Holt <galen.holt@gmail.com>
2021-08-23 21:23:05 +1000

    all catchment shell scripts, including one that passes catchment as argument. Already-run catchments are commented out

--- allCatchTemp.sh
+++ allCatchTemp.sh
@@ -1,12 +1,100 @@
-#!/bin/bash
-sbatch --array=1-100 KiewaTemp.sh
-sleep 2
-sbatch --array=1-100 LachlanTemp.sh
-sleep 2
-sbatch --array=1-100 LoddonTemp.sh
-sleep 2
-sbatch --array=1-100 LowerDarlingTemp.sh
-sleep 2
-sbatch --array=1-100 LowerMurrayTemp.sh
+# #!/bin/bash
+# echo 'start Avoca'
+# sbatch --array=1-100 AvocaTemp.sh
+# sleep 2
+
+# echo 'start BarwonDarling'
+# sbatch --array=1-100 BarwonDarlingTemp.sh
+# sleep 2
+
+# echo 'start BorderRivers'
+# sbatch --array=1-100 BorderRiversTemp.sh
+# sleep 2
+
+# echo 'start Broken'
+# sbatch --array=1-100 BrokenTemp.sh
+# sleep 2
+
+# echo 'start Campaspe'
+# sbatch --array=1-100 CampaspeTemp.sh
+# sleep 2
+
+# echo 'start Castlereagh'
+# sbatch --array=1-100 CastlereaghTemp.sh
+# sleep 2
+
+# echo 'start CentralMurray'
+# sbatch --array=1-100 CentralMurrayTemp.sh
+# sleep 2
+
+# echo 'start CondamineBalonne'
+# sbatch --array=1-100 CondamineBalonneTemp.sh
+# sleep 2
+
+# echo 'start EdwardWakool'
+# sbatch --array=1-100 EdwardWakoolTemp.sh
+# sleep 2
+
+# echo 'start Goulburn'
+# sbatch --array=1-100 GoulburnTemp.sh
+# sleep 2
+
+# echo 'start Gwydir'
+# sbatch --array=1-100 GwydirTemp.sh
+# sleep 2
+
+# echo 'start Kiewa'
+# sbatch --array=1-100 KiewaTemp.sh
+# sleep 2
+
+# echo 'start Lachlan'
+# sbatch --array=1-100 LachlanTemp.sh
+# sleep 2
+
+# echo 'start Loddon'
+# sbatch --array=1-100 LoddonTemp.sh
+# sleep 2
+
+# echo 'start LowerDarling'
+# sbatch --array=1-100 LowerDarlingTemp.sh
+# sleep 2
+
+# echo 'start LowerMurray'
+# sbatch --array=1-100 LowerMurrayTemp.sh
+# sleep 2
+
+echo 'start Macquarie'
+sbatch --array=1-100 MacquarieTemp.sh
 sleep 2
 
+echo 'start MittaMitta'
+sbatch --array=1-100 MittaMittaTemp.sh
+sleep 2
+
+echo 'start Murrumbidgee'
+sbatch --array=1-100 MurrumbidgeeTemp.sh
+sleep 2
+
+echo 'start Namoi'
+sbatch --array=1-100 NamoiTemp.sh
+sleep 2
+
+echo 'start Ovens'
+sbatch --array=1-100 OvensTemp.sh
+sleep 2
+
+echo 'start Paroo'
+sbatch --array=1-100 ParooTemp.sh
+sleep 2
+
+echo 'start UpperMurray'
+sbatch --array=1-100 UpperMurrayTemp.sh
+sleep 2
+
+echo 'start Warrego'
+sbatch --array=1-100 WarregoTemp.sh
+sleep 2
+
+echo 'start Wimmera'
+sbatch --array=1-100 WimmeraTemp.sh
+sleep 2
