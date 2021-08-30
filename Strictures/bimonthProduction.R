7abb8780915440029c6a7b170b25957fcfdc1599
Galen Holt <galen.holt@gmail.com>
2021-08-30 11:21:25 +1000

    updated to just do the volume multiplication in the bimonthing- everything's already in and next to each other with the right setup

--- Strictures/bimonthProduction.R
+++ Strictures/bimonthProduction.R
@@ -21,6 +21,10 @@ plan(multisession)
 scriptOut <- file.path(datOut, 'TempAndProduction', 'Predictions', 'bimonth')
 if (!dir.exists(scriptOut)) {dir.create(scriptOut, recursive = TRUE)}
 
+# and to save the predicted x volumes
+totalOut <- file.path(scriptOut, 'predictxvol')
+if (!dir.exists(totalOut)) {dir.create(totalOut, recursive = TRUE)}
+
 # NOTE: this same approach should be easily extensible to bimonthing the temp itself if we want
 
 # directory with metabolism predictions
@@ -42,6 +46,9 @@ catchNames
 # I'm not building anything with chunks, just saving. So, I think I can probably
 # run this locally, and use a standard for loop?
   # Though parallelizing would be nice. so maybe I will foreach it, but not return anything
+
+# For testing
+# ca <- 1
 
 # trashOut <- foreach(ca = 4:5) %dopar% {
 trashOut <- foreach(ca = 1:length(catchNames)) %dopar% {
@@ -69,33 +76,69 @@ trashOut <- foreach(ca = 1:length(catchNames)) %dopar% {
   predicttimes <- st_get_dimension_values(catchPredict, which = 'time')
   
   # Need to clip so we aren't doing anything beyond the overlapping times
+  whichoverlap <- which(inuntimes >= min(predicttimes))
   overlaptimes <- inuntimes[inuntimes >= min(predicttimes)]
+  
+  # Get the relevant volumes
+  overlapInun <- catchInun %>% slice('time', index = whichoverlap)
+  
   
   # # Can I just throw that in yearsummary? NO. But maybe tempaggregate
   # # Stolen from yearsummary
-  startdate <- min(predicttimes)
-  enddate <- max(predicttimes)
-  # 
-  by_t <- c(startdate, overlaptimes,  enddate)
+  startpredict <- min(predicttimes)
+  endpredict <- max(predicttimes)
+  endinun <- max(inuntimes)
+  # So, I need to set the intervals with the inuntimes
+  # and I need to START the intervals with the previous time, I think (though
+  # that is likely to yield NAs, because the inundation there is for the 2
+  # months PRECEDING Jan 1, and the temps don't start until Jan 1)
+  precedingInun <- max(inuntimes[inuntimes < startpredict])
+  # # 
+  # by_t <- c(startdate, overlaptimes,  enddate)
+  startdate <- max(inuntimes < min(predicttimes))
+  by_t <- c(precedingInun, overlaptimes)
   # # Now just go over to tempaggregate (NOT yearsummary- it expects more structure strictures) and step through
   
   # TAKE THE LOG OFF BEFORE AVERAGING
+    # The problem is, this ends up throwing a lot of infs
   catchPredict <- exp(catchPredict)
   names(catchPredict) <- str_remove(names(catchPredict), 'log')
+  # Double check the time naming is in fact assigning the FIRST time (ie the
+  # START of the interval), while we want the END of the interval  names(catchPredict) <- str_remove(names(catchPredict), 'log')
+  # by_test <- overlaptimes[39:41] # With 40:41 it just drops time
+  # bimonthPredict <- tempaggregate(starObj = catchPredict, by = by_test, FUN = mean, na.rm = TRUE)
+  
   bimonthPredict <- tempaggregate(starObj = catchPredict, by = by_t, FUN = mean, na.rm = TRUE)
   # Pretty quick for Avoca, anyway.
   
+  # make the names obvious what's happened
   names(bimonthPredict) <- paste0('bimonth_', names(bimonthPredict))
-  
+  # shift the dates to be the END rather than START of the interval
+  bimonthPredict <- st_set_dimensions(bimonthPredict, which = 'time', values = st_dimensions(overlapInun)[2])
   bimonthPredict
   
-  thisOutName <- paste0(thisCatch, '_PredictBimonthMean')
-  assign(thisOutName, bimonthPredict)
-  save(list = thisOutName, file = file.path(scriptOut, paste0(thisOutName, '.rdata')))
+
+# Multiply predictions by volumes -----------------------------------------
+
+  # This is probably not necessary
+  bimonthPredict <- st_transform(bimonthPredict, st_crs(overlapInun))
+  
+  # Then just do the mult. the dims are swapped, but Ops.stars seems to handle that
+  totalPredictVol <- bimonthPredict*overlapInun
+  names(totalPredictVol) <- str_replace(names(totalPredictVol), pattern = 'bimonth', replacement = 'predict_x_vol')
+  
+  # Set up to save
+  thisOutNameB <- paste0(thisCatch, '_PredictBimonthMean')
+  assign(thisOutNameB, bimonthPredict)
+  save(list = thisOutNameB, file = file.path(scriptOut, paste0(thisOutNameB, '.rdata')))
+  
+  thisOutNameT <- paste0(thisCatch, '_PredictxVol')
+  assign(thisOutNameT, totalPredictVol)
+  save(list = thisOutNameT, file = file.path(totalOut, paste0(thisOutNameT, '.rdata')))
   
   # Get aggressive about cleanup, since some things will have new names
-  rm(list = c(thisOutName, 'bimonthPredict', 'catchPredict', 'by_t', 
-              'startdate', 'enddate', 'overlaptimes', 'catchInun', 'catchPredict'))
+  rm(list = c(thisOutNameB, thisOutNameT, 'bimonthPredict', 'by_t', 
+              'startdate', 'overlaptimes', 'catchInun', 'catchPredict'))
   # Return a null dummy, since I really just want side effects
   dummy <- NULL
 }
