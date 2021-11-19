99001df046176cc5310967276417ff158a694933
Galen Holt <galen.holt@gmail.com>
2021-11-19 22:57:36 +1100

    trying to re-run Warrego volume because corrupted

---
+++ Scripts/DataProcessing/inunVolAreaFails.R
@@ -0,0 +1,44 @@
+# script to check failures of inun volume and area
+
+# script to check failures and make new shell scripts for spoonbill foraging inundation
+
+# Rscript is easier than dealing with slurm for this
+# module load R/4.0.2
+# Rscript hpc_wrap.R 'Scripts/DataProcessing/tempFails.R'
+
+
+# Header from the temperature file to retain all the directories,  --------
+source('directorySet.R')
+
+# Let's get libraries here, then sort out git then sort out making this a
+# library so we don't have to deal with all the library crap
+library(here)
+library(tidyverse)
+# library(sf)
+# library(RNetCDF) # Probably not, raster can handle geographic netCDF
+# library(stars)
+library(foreach)
+library(doFuture)
+
+# Set up parallel backend
+registerDoFuture()
+plan(sequential) # no need to parallelize, I don't think
+
+
+# Find failures and make new sh script -------------------------------------------------------------
+
+makeSHfails(outerDir = file.path(datOut, 'Inundationprocessed'),
+            summaryFuns = 'areaInun',
+            varName = 'areaInun',
+            nchunks = 100,
+            lengthOrChunk = c('short'), # , 'long', 'chunk'
+            runImmediate = FALSE,
+            forceAllCatchments = TRUE)
+
+makeSHfails(outerDir = file.path(datOut, 'Inundationprocessed'),
+            summaryFuns = 'volInun',
+            varName = 'volInun',
+            nchunks = 100,
+            lengthOrChunk = c('short'), # , 'long', 'chunk'
+            runImmediate = FALSE,
+            forceAllCatchments = TRUE)
