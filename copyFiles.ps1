732d2455696b4b51265b42105ef9d9f7fd737def
Galen Holt <galen.holt@gmail.com>
2022-01-07 16:38:32 +1100

    script to copy directories from bowen to local. Very hard-coded for now, but much faster than dragndrop

---
+++ copyFiles.ps1
@@ -0,0 +1,58 @@
+
+
+# powershell commands for copying large sets of files from bowen to local. 
+# This also leaves out the 'chunked' subdirectories.
+
+# have to use powershell because it's the only thing with access to Bowen and C
+# SO, ignore all the putty/hpc stuff, and open powershell directly
+# Then paste this in, one at a time (I guess- probably can run it as a script?)
+# I think I can probably cd "\\Client\C$\Users\Galen\Dropbox\Australia\MER\GalenGits\CC2"
+# and then ./copyFiles.ps1
+
+# The temperature version
+robocopy "\\fs1-cbr.nexus.csiro.au\{lw-mer}\work\galen_holt\datOut\TempAndProduction\Predictions" `
+"\\Client\C$\Users\Galen\Dropbox\Australia\MER\GalenGits\CC2\datOut\TempAndProduction\Predictions" `
+/E /XD dirs "chunked"
+
+# Climate
+robocopy "\\fs1-cbr.nexus.csiro.au\{lw-mer}\work\galen_holt\datOut\ClimateAndProduction\Predictions" `
+"\\Client\C$\Users\Galen\Dropbox\Australia\MER\GalenGits\CC2\datOut\ClimateAndProduction\Predictions" `
+/E /XD dirs "chunked"
+
+
+
+<# # test cp calls
+
+
+
+rsync -av /datasets/work/lw-mer/work/galen_holt/datOut/TEST //Client/C$/Users/Galen/Dropbox/Australia/MER/GalenGits/CC2/datOut/CHECK
+
+
+
+
+
+
+
+cp -rp /datastore/hol436/datOut/TempAndProduction/Predictions/logERdaysvalleys //Client/C$/Users/Galen/Dropbox/Australia/MER/GalenGits/CC2/datOut/TempAndProduction/Predictions
+
+cp -rp /datastore/hol436/datOut/TEST //Client/C$/Users/Galen/Dropbox/Australia/MER/GalenGits/CC2/datOut/CHECK
+
+
+# POWERSHELL
+robocopy "\\fs1-cbr.nexus.csiro.au\{lw-mer}\work\galen_holt\datOut\TEST" \\Client\C$\Users\Galen\Dropbox\Australia\MER\GalenGits\CC2\datOut\CHECK/TEST /E
+
+# skip an internal directory
+
+robocopy "\\fs1-cbr.nexus.csiro.au\{lw-mer}\work\galen_holt\datOut\TEST" "\\Client\C$\Users\Galen\Dropbox\Australia\MER\GalenGits\CC2\datOut\CHECK/TEST" /E /XD dirs "passcheck"
+
+# looks like it does only copy things that aren't there already
+
+# what is /z mode?
+# I think skip for now, because slow
+# https://stackoverflow.com/questions/20982968/what-is-robocopys-restartable-option
+
+# trying to do line breaks so I can readit
+
+robocopy "\\fs1-cbr.nexus.csiro.au\{lw-mer}\work\galen_holt\datOut\TEST" `
+"\\Client\C$\Users\Galen\Dropbox\Australia\MER\GalenGits\CC2\datOut\CHECK/TEST" `
+/E /XD dirs "passcheck" #>
