2507a1cd6ad097924a47abd368b974903c669acf
Galen Holt <53928785+galenholt@users.noreply.github.com>
2024-03-05 11:40:15 +1100

    paralell wrapper

---
+++ R/parallel_data.R
@@ -0,0 +1,60 @@
+#' Light parallel wrapper over [processData()]
+#'
+#' @inheritParams processData
+#'
+#' @param runframe a tibble or dataframe with columns 'catchment' and 'chunk'. if nchunks == 1, the 'chunk' column still needs to be there, but there should only be one row per catchment and chunk should always be 1
+#'
+#' @return a tibble of timings. The main output is saving the data to out_dir.
+#' @export
+#'
+parallel_data <- function(runframe,
+                          dataname,
+                          data_dir,
+                          poly_path = file.path(out_dir, "ANAEprocessed"),
+                          summaryFun,
+                          out_dir,
+                          nchunks = 100,
+                          whichcrs = 3577,
+                          maxPix = 100000,
+                          rastRollArgs = NULL,
+                          saveout = TRUE) {
+
+
+  # set up progress bar
+  p <- progressor(steps = nrow(runframe))
+
+
+  alltimes <- foreach(
+    nl = 1:nrow(runframe),
+    .combine = rbind,
+    .options.future = list(
+      seed = TRUE,
+      globals = structure(TRUE, add = summaryFun)
+    )
+  ) %dofuture% {
+
+    # get the looplevels
+    w <- runframe$catchment[nl]
+    i <- runframe$chunk[nl]
+
+    inuntab <- processData(
+      dataname = dataname,
+      data_dir = data_dir,
+      poly_path = poly_path,
+      summaryFun = summaryFun,
+      out_dir = out_dir,
+      catchment = w,
+      thischunk = i,
+      nchunks = nchunks,
+      whichcrs = whichcrs,
+      maxPix = maxpix,
+      rastRollArgs = rastRollArgs,
+    )
+
+    p(glue::glue("catchment {w} chunk {i} finished"))
+
+    inuntab # This just makes sure inuntab gets into the alltimes list
+  }
+
+  return(alltimes)
+}
