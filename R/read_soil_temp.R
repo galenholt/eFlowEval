717ec69e800c9da7b71492a81435b958669796cf
Galen Holt <53928785+galenholt@users.noreply.github.com>
2024-04-12 11:03:18 +1000

    multi-file soil temp

--- R/read_soil_temp.R
+++ R/read_soil_temp.R
@@ -4,21 +4,19 @@ read_soil_temp <- function(soil_temp_dir) {
   # Set up where the soil temp data is
   tempfile <- list.files(soil_temp_dir, pattern = '.nc')
   temppath <- file.path(soil_temp_dir, tempfile)
-  
+
   # First, use a stars object to get the overall dimensions without reading data
   # use sub because the other variable is just QC values
   soilTstars <-  read_stars(temppath, sub = "LST_Day_1km", proxy = TRUE)
-  
+
   # As long as they're proxies, we can't change their crs, so have to shift
   # everything else to them.
-  # this *usually* reads in as 4326, but sometimes doesn't on the HPC. This is
+  # this *usually* reads in as 4326, but sometimes doesn't. This is
   # dangerous, but I am telling NASA to give me this CRS, so make it so but warn
-  if(st_crs(soilTstars)$epsg != 4326) {
-    st_crs(soilTstars) <- 4326 
-    warning('changed the soil CRS, but it should be correct')
-  }
-  
+  st_crs(soilTstars) <- 4326
+
+
   pixarea <- get_pixarea(temppath, st_crs(soilTstars))
-  
+
   return(list(proxy_data = soilTstars, pixarea = pixarea))
 }
