124802e23ba54b115bc2d36713d2ee183565d44b
Galen Holt <53928785+galenholt@users.noreply.github.com>
2024-04-15 13:36:35 +1000

    add package refs

--- R/read_soil_temp.R
+++ R/read_soil_temp.R
@@ -7,16 +7,16 @@ read_soil_temp <- function(soil_temp_dir) {
 
   # First, use a stars object to get the overall dimensions without reading data
   # use sub because the other variable is just QC values
-  soilTstars <-  read_stars(temppath, sub = "LST_Day_1km", proxy = TRUE)
+  soilTstars <-  stars::read_stars(temppath, sub = "LST_Day_1km", proxy = TRUE)
 
   # As long as they're proxies, we can't change their crs, so have to shift
   # everything else to them.
   # this *usually* reads in as 4326, but sometimes doesn't. This is
   # dangerous, but I am telling NASA to give me this CRS, so make it so but warn
-  st_crs(soilTstars) <- 4326
+  sf::st_crs(soilTstars) <- 4326
 
 
-  pixarea <- get_pixarea(temppath, st_crs(soilTstars))
+  pixarea <- get_pixarea(temppath, sf::st_crs(soilTstars))
 
   return(list(proxy_data = soilTstars, pixarea = pixarea))
 }
